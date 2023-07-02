1139f383a877a679cbd3b4294088ef279d572d3e
LithishaS <138385861+LithishaS@users.noreply.github.com>
2023-07-02 21:29:57 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,5 @@
-import xlrd
+string = input("Enter the word:")
+vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+for i in range(len(string)):
+    if string[i] not in vowels:
+        print(string[i])
